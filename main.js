e2d8ac44f7841fd5db51648dd2135c692ba0df06
Madison Harris <madison.harris002@student.swic.edu>
2026-09-21 21:16:49 -0500

    add name toggle

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
+
+function TextColorChange(element) {
+$(element).toggleClass('SWIC-active');
+}
+
+function ToggleName(element) {
+  $(element).toggle();
+}
+
 $(document).ready(function () {
   $('.SWIC').on('click', function () {
-    $(this).toggleClass('SWIC-active');
+    TextColorChange(this);
   });
+  function ToggleName(element) {
+  $(element).toggle();
+}
 });
 
 $('.SWIC').length
 
-
  $(document).ready(function() {
       alert("Hello World");
          });
+
+
